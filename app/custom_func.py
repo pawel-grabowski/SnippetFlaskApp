de0b7b5af8a9ee48e2b0ff127583bb0e37187f65
pawel-grabowski <grabowski.pk@gmail.com>
2020-03-29 12:17:00 +0200

    Working search function - for left pane

---
+++ app/custom_func.py
@@ -0,0 +1,83 @@
+import re
+import os
+
+def read_files(file_path):
+    with open(file_path) as f:
+        content=f.read()
+    return content
+    
+    
+def split_files(content):
+    # podzial na fragment wg z znacznika
+    content = content.replace(";\n",";\n<stop>")
+    splitted = content.split("<stop>")
+    snippets = []
+    
+    for sn in splitted:
+        header = re.search('\/\*(.+?)\*\/', sn)
+        
+        header = header.group(1)
+        
+        snippet = {"title": header, "code": sn}
+        snippets.append(snippet)
+    
+    return snippets
+    
+
+def get_titles_list(data):
+    titles_list=[]
+    for record in data:
+        titles_list.append(record["title"])
+        
+    return titles_list
+
+def get_snippet_dict(data):
+    snippets={}
+    for record in data:
+        snippets.update({record["title"] : record["code"]})
+        
+    return snippets
+
+
+def load_all_files(folder_path):
+    files=os.listdir(folder_path)
+    snippets_by_file = {}
+    
+    for file in files:
+        file_path=os.path.join(folder_path,file)
+        
+        # przejście do kolejnej iteracji jeśli *file* jest folderem
+        if os.path.isdir(file_path):
+            continue
+        
+        
+        temp = read_files(file_path)
+        temp = split_files(temp)
+        snippet_dict = get_snippet_dict(temp)
+        snippets_by_file.update({file:snippet_dict}) # dictionary of dictionaries
+    
+    return snippets_by_file
+
+def get_keys(dict): 
+    return dict.keys() 
+
+
+def get_inner_dict(dict):
+    snippets_by_title={}
+    
+    for k in dict.keys():
+        snippets_by_title.update(dict[k])
+        
+    return  snippets_by_title
+
+
+if __name__ == "__main__":
+    # a = read_files("C:/Users/Paweł/Documents/Projekty/SnippetFlaskApp/snippets/test.sql")
+    # b = split_files(a)
+    # c = get_titles_list(b)
+    # d = get_snippet_dict(b)
+
+    x = load_all_files("C:/Users/Paweł/Documents/Projekty/SnippetFlaskApp/snippets/")
+    print(get_keys(x))
+    y = get_inner_dict(x)
+   
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,24 @@
 from app import app
 from flask import render_template
-
+from app import custom_func as cf
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = {'username': 'Miguel'}
-    list = ['aaa','bbb','ccc']
-    return render_template('index.html', title='Home', user=user, list=list)
+    user = {'username': 'Pawel'}
+    file_path = "C:/Users/Paweł/Documents/Projekty/SnippetFlaskApp/snippets/"
+    snippets_dict = cf.get_inner_dict(cf.load_all_files(file_path))
+    
+    list = cf.get_keys(snippets_dict) #['aaa','bbb','ccc']
     
     
+    return render_template('index.html',
+                           title='Simple SQL snippet manager',
+                           user=user,
+                           list=list,
+                           content=snippets_dict)
+
 # @app.route('/show_snippet')
 # def show_snippet():
 #     #gist=
